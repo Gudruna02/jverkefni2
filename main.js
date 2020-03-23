6c2fd049a194d8337f5eb7e9260d2a1d7493cb08
Anh Thu Vu <42570268+anhthuvu@users.noreply.github.com>
2020-03-23 16:36:02 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ function Quiz(questions) {
     this.questionIndex = 0;
 }
 
+// Ná í spurningu
 Quiz.prototype.getQuestionIndex = function() {
     return this.questions[this.questionIndex];
 }
 
+// Fá stig ef svara rétt
 Quiz.prototype.guess = function(answer) {
     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
@@ -16,10 +18,10 @@ Quiz.prototype.guess = function(answer) {
     this.questionIndex++;
 }
 
+// Svara öllum spurningum
 Quiz.prototype.isEnded = function() {
     return this.questionIndex === this.questions.length;
 }
-
 
 function Question(text, choices, answer) {
     this.text = text;
@@ -27,42 +29,49 @@ function Question(text, choices, answer) {
     this.answer = answer;
 }
 
+// Svarar rétt
 Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
 
-
 function populate() {
+    // Sýna stig ef búið
     if(quiz.isEnded()) {
         showScores();
     }
     else {
-        // show question
+        // Sýna sprningu
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
 
-        // show options
+        // Sýna möguleika
         var choices = quiz.getQuestionIndex().choices;
         for(var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
+            if(choices.length == 4){
+                document.getElementById("btn2").style.visibility = "visible";
+                document.getElementById("btn3").style.visibility = "visible";
+			}
             if(choices.length == 2){
                 document.getElementById("btn2").style.visibility = "hidden";
                 document.getElementById("btn3").style.visibility = "hidden";
 			}
         }
-
+        // Kalla í föll showProgress
         showProgress();
     }
 };
 
 function guess(id, guess) {
     var button = document.getElementById(id);
+    // Þegar ýta á button
     button.onclick = function() {
+        // Breyta um lit
         document.getElementById(id).style.backgroundColor = "#0149ff";
         quiz.guess(guess);
-        var delayInMilliseconds = 1000; //1 second
+        var delayInMilliseconds = 1000; //1 sek
         setTimeout(function() {
         document.getElementById(id).style.backgroundColor = "#01BBFF";
         populate()
@@ -70,13 +79,14 @@ function guess(id, guess) {
     }
 };
 
-
+// Sýna í hvaða spurningu
 function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("progress");
     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
 
+// Sýna stig
 function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
@@ -84,14 +94,13 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
 
-// create questions here
+// Búa til nýja spurningar
 var questions = [
     new Question("What is 1+1?", ["0", "1", "2", "11"], "2"),
     new Question("Is the Earth a sphere?", ["Yes", "No"], "Yes"),
 ];
 
-// create quiz
+// Búa til quiz
 var quiz = new Quiz(questions);
 
-// display quiz
 populate();
